b8cc990b7123784e5853466451cc8b99837d4ce8
Juanjo Ruiz <juanjoserusa@gmail.com>
2024-11-15 19:25:45 +0100

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React,{ useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-warning'>Hola mundo</h1>
+    </>
+  )
+}
+
+export default App
